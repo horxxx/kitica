02fd6c699ce3da32f15d753644f466bc4b51c49f
domac <domac@DESKTOP-9CPPBVB>
2022-05-04 20:59:22 +0200

    Add project files.

---
+++ ConsoleApp1/Class1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class User
+        {
+            List<User> _userList = new List<User>();
+            private string _ime, _prezime;
+        private float tezina;
+        private float visina;
+
+        public string Ime { get => _ime; set => _ime = value; }
+        public string Prezime { get => _prezime; set => _prezime = value; }
+        public float Visina { get => visina; set => visina = value; }
+        public float Tezina { get => tezina; set => tezina = value; }
+
+        public User( string ime, string prezime, float visina, float tezina)
+            {
+                Ime = ime;
+                Prezime = prezime;
+                Visina = visina;
+                Tezina = tezina;
+            }
+
+            public string GetIme()
+            {
+                return Ime;
+            }
+
+            public void SetIme(string ime)
+            {
+                Ime = ime;
+            }
+
+            public string GetPrezime()
+            {
+                return Prezime;
+            }
+
+            public void SetPrezime(string prezime)
+            {
+                Prezime = prezime;
+            }
+
+            public float GetVisina()
+            {
+                return Visina;
+            }
+
+            public void SetVisina(float visina)
+            {
+                Visina = visina;
+            }
+
+            public float GetTezina()
+            {
+                return Tezina;
+            }
+
+            public void SetTezina(float tezina)
+            {
+                Tezina = tezina;
+            }
+
+        }
+    }
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,43 @@
+﻿using ConsoleApp1;
+using System;
+
+namespace Simple
+{
+
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            List<User> _userList = new List<User>();
+            Console.Write("\nUnesite broj korisnika: ");
+            int b = int.Parse(Console.ReadLine());
+            float c = 0;
+            float avg = 0;
+            for (int i = 0; i < b; i++)
+            {
+                Console.Write("Ime: ");
+                string ime = Console.ReadLine();
+
+                Console.Write("Prezime: ");
+                string prezime = Console.ReadLine();
+
+                Console.Write("Visina: ");
+                float visina = float.Parse(Console.ReadLine());
+
+                Console.Write("Tezina: ");
+                float tezina = float.Parse(Console.ReadLine());
+
+                User user = new User(ime, prezime, visina, tezina);
+                _userList.Add(user);
+            }
+            foreach (User u in _userList)
+            {
+                c += u.Visina;
+                avg = c / b;
+            }
+            Console.WriteLine(avg);
+        }
+    }
+}
